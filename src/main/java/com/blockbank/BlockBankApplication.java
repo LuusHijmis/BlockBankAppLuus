1b14598a56504c61c866393e3b9f330f8fbfbb21
stevenh <harold.stevens@hva.nl>
2021-08-20 16:02:49 +0200

    startproject

---
+++ src/main/java/com/blockbank/BlockBankApplication.java
@@ -0,0 +1,13 @@
+package com.blockbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlockBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blockbank/BlockBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blockbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
