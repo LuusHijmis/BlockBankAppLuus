4add8071dde426d0497b532450424eab581e5525
Hannah <hvdam@live.nl>
2021-08-23 20:20:41 +0200

    Portfolio model created (should be checked)

---
+++ src/main/java/com/blockbank/database/domain/Portfolio.java
@@ -0,0 +1,59 @@
+package com.blockbank.database.domain;
+
+/**
+ * @author hannahvd
+ */
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class Portfolio {
+
+    private final Logger logger = LoggerFactory.getLogger(Portfolio.class);
+
+    private int clientId; //should be linked to Client model?
+    private Map<Asset, Integer> assetList;
+
+    public Portfolio(int clientId) {
+        super();
+        this.clientId = clientId;
+        assetList = new HashMap<>();
+        logger.info("New portfolio created for client " + clientId);
+    }
+
+    public void addToAssetList(Asset asset, int amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Amount must be 1 or more");
+        }
+        this.assetList.put(asset, amount);
+    }
+
+    public int getClientId() {
+        return clientId;
+    }
+
+    @Override
+    public String toString() {
+        return "Portfolio{" +
+                "clientId=" + clientId +
+                ", assetList=" + assetList +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Portfolio portfolio = (Portfolio) o;
+        return clientId == portfolio.clientId && assetList.equals(portfolio.assetList);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(clientId, assetList);
+    }
+}
