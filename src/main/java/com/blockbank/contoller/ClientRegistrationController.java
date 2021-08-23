c9230bd9edd615781d1f39134079cda3d4f3c143
Karish Resodikromo <karish.resodikromo@hva.nl>
2021-08-23 20:21:33 +0200

    RequestBody in PostMapping gewijzigd

--- src/main/java/com/blockbank/contoller/ClientRegistrationController.java
+++ src/main/java/com/blockbank/contoller/ClientRegistrationController.java
@@ -6,9 +6,12 @@ package com.blockbank.contoller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 
 @RestController
@@ -26,11 +29,13 @@ public class ClientRegistrationController {
     }
 
     //TODO ClientRegistrationService maken + register methode
-    //TODO PostMapping afmaken
+    //TODO PostMapping checken
     @PostMapping("/register")
-    public User register(@RequestParam User user) {
-        return clientregistrationservice.register(user);
+    ResponseEntity<?> registerUser(@RequestBody User user) {
+        clientregistrationservice.register(user);
+        URI uri = URI.create(String.format("http://miw-team-4.nl/users/%d", user.getId()));
+        return ResponseEntity.created(uri)
+                .body(user);
     }
-
 }
 
