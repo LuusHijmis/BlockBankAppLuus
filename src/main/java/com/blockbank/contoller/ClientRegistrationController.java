90222a9d6263f00bf77eddfb096430838bfc0c2d
Karish Resodikromo <karish.resodikromo@hva.nl>
2021-08-25 12:28:06 +0200

    ClientRegistrationController aangepast n.a.v. tussentijdse review1 Huub

--- src/main/java/com/blockbank/contoller/ClientRegistrationController.java
+++ src/main/java/com/blockbank/contoller/ClientRegistrationController.java
@@ -1,18 +1,18 @@
-/*
+
 package com.blockbank.contoller;
 
-*/
+
 /*@author Karish Resodikromo
- * Deze klasse behelst requesthandlers voor registreren van klanten *//*
+ * Deze klasse behelst requesthandlers voor registreren van klanten */
 
 
+import com.blockbank.database.domain.UserDTO;
+import com.blockbank.service.ClientRegistrationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
@@ -27,19 +27,19 @@ public class ClientRegistrationController {
 
     @Autowired
     public ClientRegistrationController(ClientRegistrationService clientRegistrationService) {
-        this.clientregistration = clientRegistrationService;
+        this.clientregistrationservice = clientRegistrationService;
         logger.info("New ClientRegistrationController");
     }
 
-    //TODO ClientRegistrationService maken + register methode
+
     //TODO PostMapping checken
-    @PostMapping("/register")
-    ResponseEntity<?> registerUser(@RequestBody User user) {
-        clientregistrationservice.register(user);
-        URI uri = URI.create(String.format("http://miw-team-4.nl/users/%d", user.getId()));
+    @PutMapping("/register")
+    public ResponseEntity<?> register (@RequestBody UserDTO userDTO) {
+        clientregistrationservice.register(userDTO);
+        URI uri = URI.create(String.format("http://miw-team-4.nl/users/%d", userDTO.getUsername()));
         return ResponseEntity.created(uri)
-                .body(user);
+                .body(userDTO);
     }
 }
 
-*/
+
