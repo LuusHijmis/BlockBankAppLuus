1f02d31f8d9ddb4742ff881f02b05d857f89d732
Hannah <hvdam@live.nl>
2021-08-26 17:28:30 +0200

    test tweak (still todo)

--- src/test/java/com/blockbank/service/PasswordValidationServiceTest.java
+++ src/test/java/com/blockbank/service/PasswordValidationServiceTest.java
@@ -37,35 +37,18 @@ class PasswordValidationServiceTest {
 
     static Stream<String> validPasswordProvider() {
         return Stream.of(
-                "12345678901aB45678@1234");
-//                "mkyong",
-//                "javaregex",
-//                "JAVAregex",
-//                "java.regex",
-//                "java-regex",
-//                "java_regex",
-//                "java.regex.123",
-//                "java-regex-123",
-//                "java_regex_123",
-//                "javaregex123",
-//                "123456",
-//                "java123",
-//                "01234567890123456789");
+                "12345678901aB45678@1234",
+                "HOOFDLETTERs.1",
+                "kleineletterS.1",
+                " spaaaTi3ss. ",
+                "_!heLLo0ooo!_#",
+                "012345aBBa567.9");
     }
 
     static Stream<String> invalidPasswordProvider() {
         return Stream.of(
-                "aBc1.",                        //invalid length 3; length must between 12-24
-                "012345678901pPdS@!£23456789a", //invalid length 21; length must between 12-24
-                "_javaregex_",                  //invalid start and last character
-                ".javaregex.",                  //invalid start and last character
-                "-javaregex-",                  //invalid start and last character
-                "javaregex#$%@123",             //invalid symbols, support dot, hyphen and underscore
-                "java..regex",                  //dot cant appear consecutively
-                "java--regex",                  //hyphen can't appear consecutively
-                "java__regex",                  //underscore can't appear consecutively
-                "java._regex",                  //dot and underscore can't appear consecutively
-                "java.-regex",                   //dot and hyphen can't appear consecutively
+                "aBc1.",                        //invalid length 4; length must between 12-24
+                "012345678901pPdS@!£23456789a", //invalid length 24+; length must between 12-24
                 " ",                            //empty
                 "");                            //empty
     }
