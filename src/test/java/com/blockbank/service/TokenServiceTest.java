c38ac277e53a021d9f9eceffb2715df72f647764
Alex Shiyan <alex.shijan@hva.nl>
2021-09-02 16:46:46 +0200

    TokenService en TokenServiceTest geupdate met JWT encoding.

--- src/test/java/com/blockbank/service/TokenServiceTest.java
+++ src/test/java/com/blockbank/service/TokenServiceTest.java
@@ -4,6 +4,9 @@ package com.blockbank.service;
  * @author Alex Shijan
  */
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.blockbank.database.domain.Address;
 import com.blockbank.database.domain.ClientDetails;
 import com.blockbank.database.domain.UserDetails;
@@ -13,6 +16,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDate;
+import java.util.Map;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -22,19 +28,20 @@ class TokenServiceTest {
     TokenService tokenService;
 
     @Test
-    void issueToken() {
-        ClientDetails clientDetails = new ClientDetails("Harold", "","Stevens",
-                LocalDate.parse("1973-09-25"),123456007,"info@hjstevens.nl");
-        Address address = new Address("Pieter Woutersstraat",26,null,"2215MC",
-                "Voorhout","");
-        UserDetails tokenUser = new UserDetails("Harold","dlorah","123",clientDetails,address);
-        tokenUser.setUserID(1);
+    void issueTokenWithCorrectClaims() {
+        String token = tokenService.issueToken("Harold");
+        DecodedJWT decodedJWT = JWT.decode(token);
 
-        String expected = String.format(
-                "{\"iss\": \"%s\",\"iat\": %d,\"exp\": %d,\"aud\": \"%s\",\"sub\": \"%s\",\"Role\": \"%s\"}"
-                , "Blockbank", 1630497103, 1630498301, "www.blockbank.com", tokenUser.getUsername(),
-                tokenUser.getRole());
-        String actual = tokenService.issueToken("Harold");
-        System.out.println(actual);
+        Map<String, Claim> actualClaims = decodedJWT.getClaims();
+        String expected = "{iss=\"Blockbank\", role=\"client\", username=\"Harold\"}";
+
+        assertThat(actualClaims.toString()).isEqualTo(expected);
+    }
+
+    @Test
+    void verifyTokenTest() {
+        String token = tokenService.issueToken("Harold");
+
+        assertThat(tokenService.verifyToken(token)).isEqualTo(true);
     }
 }
