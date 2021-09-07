783da6e28cff6f1ba0565ace3aa660a11ddba2c5
Alex Shiyan <alex.shijan@hva.nl>
2021-09-07 12:26:04 +0200

    AssetControllerTest aangemaakt.

---
+++ src/test/java/com/blockbank/contoller/AssetControllerTest.java
@@ -0,0 +1,15 @@
+package com.blockbank.contoller;
+/**
+ * @author Alex Shijan
+ */
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AssetControllerTest {
+
+    @Test
+    void assetList() {
+    }
+}
