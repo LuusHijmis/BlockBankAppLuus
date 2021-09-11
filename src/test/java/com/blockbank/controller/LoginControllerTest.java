d06ad1bba725e43d3643b9789e290447f3190299
Hannah <hvdam@live.nl>
2021-09-11 17:36:46 +0200

    controller test no success

---
+++ src/test/java/com/blockbank/controller/LoginControllerTest.java
@@ -0,0 +1,82 @@
+package com.blockbank.controller;
+
+import com.blockbank.contoller.LoginController;
+import com.blockbank.database.repository.RootRepository;
+import com.blockbank.service.LoginService;
+import com.blockbank.service.LoginValidationService;
+import com.blockbank.service.TokenService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @author hannahvd
+ */
+
+/*
+https://rieckpil.de/guide-to-testing-spring-boot-applications-with-mockmvc/
+https://spring.io/guides/gs/testing-web/
+https://reflectoring.io/spring-boot-web-controller-test/ <<<
+ */
+
+@WebMvcTest(LoginController.class)
+class LoginControllerTest {
+
+    //private RootRepository rootRepository;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webAppContext;
+
+    @MockBean
+    private LoginService loginService;
+
+    @MockBean
+    private TokenService tokenService;
+
+//    @MockBean
+//    private LoginValidationService loginValidationService;
+
+    @BeforeEach
+    void setup() throws Exception {
+        this.mockMvc = MockMvcBuilders
+                .webAppContextSetup(this.webAppContext)
+                .build();
+    }
+
+    @Test
+    void verifyHTTPrequest() throws Exception {
+        mockMvc.perform(post("/login")
+                .contentType("application/json")) //? //Set the 'Content-Type' header of the request as a raw String value
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void mockLoginService() {
+        when(loginService.login("hannahvd", "hannahVD123."))
+                .thenReturn(tokenService.issueToken("hannahvd")); //return niet ok?
+    }
+
+    @Test
+    void login() throws Exception {
+        when(loginService.login("hannahvd", "hannahVD123.")).thenReturn("jwtUser");
+
+        mockMvc.perform(post("/login"))
+                .andDo(print())
+                .andExpect(status().isOk());
+                //.andExpect(content().string(containsString("Incorrect credentials")));
+    }
+
+}
