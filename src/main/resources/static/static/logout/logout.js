29868970fb731eec165f139f06e535f3793f334e
Karish Resodikromo <karish.resodikromo@hva.nl>
2021-09-14 10:39:26 +0200

    welcome.html foutje gewijzigd en onnodige comments uit registration controller

---
+++ src/main/resources/static/static/logout/logout.js
@@ -0,0 +1,18 @@
+window.addEventListener('load',main)
+
+function main() {
+
+
+
+    fetch('http://localhost:8080/logout',
+        {
+            method: 'GET',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify
+        })
+        .then(data => {
+            localStorage.removeItem('Authentication', data);
+            window.location.pathname = '/static/index/index.html';
+        })
+}
+
