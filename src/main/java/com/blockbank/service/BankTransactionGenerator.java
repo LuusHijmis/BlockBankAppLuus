a493402a686b4435d3c9842e75feeec9c8807ea1
Alex Shiyan <alex.shijan@hva.nl>
2021-09-15 10:28:20 +0200

    BankTransactionGenerator bijgewerkt met checks en juiste ids.

--- src/main/java/com/blockbank/service/BankTransactionGenerator.java
+++ src/main/java/com/blockbank/service/BankTransactionGenerator.java
@@ -4,7 +4,7 @@ package com.blockbank.service;
  * @author Alex Shijan
  */
 
-import com.blockbank.contoller.AssetController;
+
 import com.blockbank.database.domain.Asset;
 import com.blockbank.database.domain.Transaction;
 import com.blockbank.database.domain.TransactionDTO;
@@ -14,11 +14,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.awt.*;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
+
 import java.util.List;
 
 @Service
@@ -38,17 +35,18 @@ public class BankTransactionGenerator {
         String transactionSort = "Buy";
         LocalDateTime time = LocalDateTime.now();
         Double transactionFee = 0.00;
-        int opposingUserId = 9999;
+        int opposingUserId = 2;
+        UserDetails adminstrator= rootRepository.findUserByUsername(admin);
         List<Asset> assetList = rootRepository.showAllAssets();
-        if(adminCheck(admin)==true) {
-            UserDetails adminstrator= rootRepository.findUserByUsername(admin);
+        List<Transaction> transactions = rootRepository.findTransactionsByUSerID(adminstrator.getUserID());
+        if(adminCheck(admin)==true && transactions == null) {
             for(Asset asset : assetList) {
                 rootRepository.saveTransaction(new TransactionDTO(adminstrator.getUserID(), time,
                         transactionSort, randomNumberGenerator() ,asset.getExchangeRate(), transactionFee,
                         opposingUserId, asset.getAssetID()));
             }
         } else {
-            System.out.println("Bank does not exist");
+            System.out.println("Bank does not exist or it already has starting transactions.");
         }
     }
 
