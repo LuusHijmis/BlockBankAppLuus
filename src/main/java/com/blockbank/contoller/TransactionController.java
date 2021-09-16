1e6627ed17bb034a53f29c54ed0f1618059d7bd9
Lucien Hijmissen <lucien.hijmissen@hva.nl>
2021-09-16 16:54:43 +0200

    portfolio.js uitgebreid met sell optie

--- src/main/java/com/blockbank/contoller/TransactionController.java
+++ src/main/java/com/blockbank/contoller/TransactionController.java
@@ -1,7 +1,11 @@
 package com.blockbank.contoller;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.blockbank.database.domain.PortfolioDTO;
 import com.blockbank.database.domain.Transaction;
 import com.blockbank.database.domain.TransactionDTO;
+import com.blockbank.service.TokenService;
 import com.blockbank.service.TransactionService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.slf4j.Logger;
@@ -11,27 +15,42 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 public class TransactionController {
 
    private TransactionService transactionService;
+   private TokenService tokenService;
     private final Logger logger = LoggerFactory.getLogger(TransactionController.class);
 
     @Autowired
-    public TransactionController(TransactionService transactionService) {
+    public TransactionController(TransactionService transactionService, TokenService tokenService) {
         this.transactionService = transactionService;
+        this.tokenService = tokenService;
         logger.info("New TransactionController");
     }
     @PostMapping("/transaction")
-    public ResponseEntity<?> storeTransaction(@RequestBody TransactionDTO transactionDTO) {
-       Transaction transaction = transactionService.storeTransaction(transactionDTO);
-        if (transaction != null) {
-            URI uri = URI.create(String.valueOf(transaction.getTransactionID()));
-            // front-end related
-            return ResponseEntity.created(uri).build();
+    public ResponseEntity<?> storeTransaction(@RequestHeader (name="Authorization") String token,
+                                              @RequestHeader (name="transactionSort") String transactionSort,
+                                              @RequestHeader (name="amount") double amount,
+                                              @RequestHeader (name="opposingUserID") int oppossingUserID,
+                                              @RequestHeader (name="assetID") String assetID) {
+        if(tokenService.verifyToken(token)) {
+            DecodedJWT decodedJWT = JWT.decode(token);
+            String username = decodedJWT.getClaim("username").asString();
+            int userID = transactionService.rootRepository.findUserByUsername(username).getUserID();
+            System.out.println(userID);
+            double exchangeRate = transactionService.rootRepository.findAssetById(assetID).getExchangeRate();
+            double transactionCost= 0.5;
+            TransactionDTO transactionDTO = new TransactionDTO(userID, LocalDateTime.now(),transactionSort,amount,
+                    exchangeRate,transactionCost,oppossingUserID,assetID);
+            transactionService.storeTransaction(transactionDTO);
+
+            return ResponseEntity.status(201).build();
         } else {
-            return ResponseEntity.badRequest().build();
+            return ResponseEntity.status(403).build();
         }
     }
 
--- src/main/resources/static/static/portfolio/portfolio.js
+++ src/main/resources/static/static/portfolio/portfolio.js
@@ -1,4 +1,4 @@
-window.onload= changeTabel;
+
 document.addEventListener('DOMContentLoaded', main);
 function main() {
     $.ajax({
@@ -13,10 +13,7 @@ function main() {
             console.log(status);
             console.log(xhr);
             CreateTableFromJSON(data);
-
-            // changeTabel();
-            // addBuyButtons();
-
+            document.getElementById("Sell").addEventListener('click', sellRequest)
             alert('request successful');
             //data opslaan in een variabele
             //
@@ -30,64 +27,7 @@ function main() {
     })
 
 }
-function changeTabel(){
-
-    document.getElementsByTagName('th').item(0).innerHTML="Asset ID";
-    document.getElementsByTagName('th').item(1).innerHTML="Name";
-    document.getElementsByTagName('th').item(2).innerHTML="Symbol";
-    document.getElementsByTagName('th').item(3).innerHTML="Exchange rate (EUR)";
-
-}
-
-function addBuyButtons() {
-    var data =document.getElementsByTagName('td');
-    var tables = document.getElementsByTagName('table');
-    // var headers= document.getElementsByTagName('hd');
-    var row = document.getElementsByTagName('tr').item(1);
-    // var table = tables.item(0);
-    console.log(row);
-
-    var rows = document.getElementsByTagName('tr');
-
-    for (let i = 1; i < rows.length; i++) {
-        var row = document.getElementsByTagName('tr').item(i);
-        var rowCell = row.cells[5];
-        var rowCellAssetID= row.cells[1];
-        console.log(rowCellAssetID.firstChild.nodeValue);
-        var id = "buyButton" + i;
-        var buttonString = "<button id='buyButton" + i + "'class=\"button-click\">Buy</button>";
-        console.log(id);
-        console.log(buttonString);
-        rowCell.innerHTML = buttonString;
-        document.getElementById(id).addEventListener("click", () => {
-            var dataValue = $(this).attr('assetID');
-            var filteredResults = results.filter(function (result) {
-                if (result.IdNum.toString() === dataValue.toString()) {
-                    return result;
-                }
-            });
-
-            console.log(filteredResults);
-            // var huidigeRij = de listener moet achterhalen aan welke rij hij gekoppeld is
-            // var huidigeAssetID = huidigeRij.cells[1];
-            // var assetID = rowCellAssetID.firstChild.nodeValue;
-            // console.log(assetID);
-        console.log(alert(rowCellAssetID.firstChild.nodeValue));
-        });
-
-
-
-        // tables.onclick = e => {
-        //     if (!e.target.matches("button.buyButton1")) return console.log(e.target.textContent);
-        // }
-    }
-
-}
-
-
-
-
-    function CreateTableFromJSON(data) {
+function CreateTableFromJSON(data) {
 
         var results = JSON.parse(data);
 
@@ -102,8 +42,8 @@ function addBuyButtons() {
                 // add a class to the table cell here and also another attr, which we will call data-value which will contain 			the 	value of the data.
                 resultsHtml += '<tr><td dataValue="' + result.assetID + '" class="table-cell-action">' + result.assetID + '</td>' +
                     '<td>' + result.assetName + '</td><td>' + result.assetSymbol + '</td><td> ' + result.exchangeRate + '</td> ' +
-                    '<td>' + result.assetAmount + ' </td><td><button data-id="' + result.assetID + '" class="button-click">Buy</button></td>' +
-                    '<td><button data-id="' + result.assetID + '" class="button-click">Sell</button></td></tr>'
+                    '<td>' + result.assetAmount + ' </td><td><button data-id="' + result.assetID + '" data-action="Buy" ' + ' class="button-click">Buy</button></td>' +
+                    '<td><button data-id="' + result.assetID + '" data-action="Sell" '+ ' class="button-click">Sell</button></td></tr>'
             });
 
             resultsHtml += '</table>'
@@ -145,57 +85,44 @@ function addBuyButtons() {
             var selectedRowValues = filteredResults[0];
 
 // this will contain all the values for that row
-            console.log(selectedRowValues)
-
-            ;
-            // var dataValue = $(this).attr('data-url'); // dont't need this any more
-            alert(selectedRowValues.assetName + " " + selectedRowValues.assetID);
+            if($(this).attr('data-action') === "Sell") {
+                document.getElementById("assetID").innerHTML = "AssetID: " + selectedRowValues.assetID;
+                document.getElementById("assetName").innerHTML = "Asset Name: " + selectedRowValues.assetName;
+                document.getElementById("exchangeRate").innerHTML = "Price per asset: " + selectedRowValues.exchangeRate;
+                document.getElementById("transactionCosts").innerHTML = "Transaction costs: ";
+            } else {
+                window.location.replace("http://localhost:8080/static/transaction/transaction.html");
+            }
         });
+}
+function sellRequest(){
+    var transactionSort = "Sell";
+    var transactionAmount = document.getElementById(sellAmount_f).value;
+    var opposingUserID = 1;
+    var assetID = document.getElementById(assetID).value;
 
+    $.ajax({
+        type : "POST",
+        url : 'http://localhost:8080/transaction',
+        headers: {"Authorization": localStorage.getItem('Authentication'), "transactionSort": transactionSort,
+            "amount" : transactionAmount, "oppossingUserID" : opposingUserID, "assetID": assetID },
+        // data : { transactionSort: transactionSort, amount : transactionAmount, oppossingUserID : opposingUserID, assetID: assetID},
+        contentType: "application/json",
+        accepts: { json: "application/json"},
+        success: function (data, status, xhr) {
+            console.log(data);
+            console.log(status);
+            console.log(xhr);
+            alert('request successful');
 
-// var myAssets = JSON.parse(data);
-        // // EXTRACT VALUE FOR HTML HEADER.
-        // // ('Asset ID', 'Symbol''Name', 'Disription' and 'exchangerate')
-        // var col = [];
-        // for (var i = 0; i < myAssets.length; i++) {
-        //     for (var key in myAssets[i]) {
-        //         if (col.indexOf(key) === -1) {
-        //             col.push(key);
-        //         }
-        //     }
-        // }
-        //
-        // col.push("Buy");
-        // col.push("Sell");
-        //
-        // // CREATE DYNAMIC TABLE.
-        // var table = document.createElement("table");
-        //
-        // // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
-        //
-        // var tr = table.insertRow(-1);                   // TABLE ROW.
-        //
-        // for (var i = 0; i < col.length; i++) {
-        //     var th = document.createElement("th");      // TABLE HEADER.
-        //     th.innerHTML = col[i];
-        //     tr.appendChild(th);
-        // }
-        //
-        // // ADD JSON DATA TO THE TABLE AS ROWS.
-        // for (var i = 0; i < myAssets.length; i++) {
-        //
-        //     tr = table.insertRow(-1);
-        //
-        //     for (var j = 0; j < col.length; j++) {
-        //         var tabCell = tr.insertCell(-1);
-        //         tabCell.innerHTML = myAssets[i][col[j]];
-        //     }
-        //
-        // }
-        //
-        // // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
-        // var divContainer = document.getElementById('showData');
-        // divContainer.innerHTML = "";
-        // divContainer.appendChild(table);
-
-    }
+        },
+        fail: function(xhr, status, error){
+            console.log(xhr);
+            console.log(status);
+            console.log(error);
+        },
+        error: function (request) {
+            alert(request.responseText);
+        }
+    });
+}
